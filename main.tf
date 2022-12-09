bb9f49738f304d6c9e024c869cb5707a78a7a275
Philip Kupsch <pkupsch@wayfair.com>
2022-12-09 04:57:30 -0500

    attach secgroup allow traffic to ptg-instance

--- main.tf
+++ main.tf
@@ -2,9 +2,10 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "ptg-instance" {
   ami = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ptg-allow-web-traffic.id]
 
   tags = {
     Name = "ptg-instance"
@@ -21,7 +22,7 @@ resource "aws_instance" "example" {
   user_data_replace_on_change = true
 }
 
-resource "aws_security_group" "instance" {
+resource "aws_security_group" "ptg-allow-web-traffic" {
   name = "ptg-allow-web-traffic"
 
   ingress {
